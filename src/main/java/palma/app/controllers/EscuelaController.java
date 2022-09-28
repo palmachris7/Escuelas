167722453b8793fd6031846b85d258990de132d9
palmachris7 <palmachristopher7@hotmail.com>
2022-09-27 19:54:53 -0500

    Login

--- src/main/java/palma/app/controllers/EscuelaController.java
+++ src/main/java/palma/app/controllers/EscuelaController.java
@@ -21,10 +21,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 import palma.app.model.Escuela;
+import palma.app.model.Perfil;
+import palma.app.model.Usuario;
 import palma.app.service.EscuelaConsumerService;
+import palma.app.service.IUsuariosService;
 import springfox.documentation.annotations.ApiIgnore;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/")
@@ -35,6 +46,13 @@ public class EscuelaController {
 	
 	@Autowired
 	private EscuelaConsumerService consumerService;
+
+	@Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+   	private IUsuariosService serviceUsuarios;
+
 		
 	@GetMapping({"/", "/escuelas"})
 	public String listadoEscuelas(Model model) {
@@ -103,6 +121,70 @@ public class EscuelaController {
 		consumerService.eliminarEscuela(id);
 		return "redirect:/escuelas";
 	} 
+
+	@GetMapping("/index")
+	public String mostrarIndex(Authentication authentication, HttpSession session) {		
+		
+		String username = authentication.getName();		
+		
+		for(GrantedAuthority rol: authentication.getAuthorities()) {
+			System.out.println("ROL: " + rol.getAuthority());
+		}
+		
+		if (session.getAttribute("usuario") == null){
+			Usuario usuario = serviceUsuarios.buscarPorUsername(username);	
+			System.out.println("Usuario: " + usuario);
+			 session.setAttribute("usuario", usuario);
+		}
+		
+		return "redirect:/";
+	}
+
+	@GetMapping("/signup")
+	public String registrarse(Usuario usuario) {
+		return "formRegistro";
+	}
+
+	@PostMapping("/signup")
+	public String guardarRegistro(Usuario usuario, RedirectAttributes attributes) {
+		String pwdPlano = usuario.getPassword();
+		String pwdEncriptado = passwordEncoder.encode(pwdPlano); 
+		usuario.setPassword(pwdEncriptado);	
+		Perfil perfil = new Perfil();
+		usuario.setEstatus(1);
+    	Date date = new Date();  
+		usuario.setFecharegistro(date);
+		perfil.setIdperfil(2); 
+		usuario.agregar(perfil);
+		serviceUsuarios.guardar(usuario);	
+		attributes.addFlashAttribute("msg", "Has sido registrado. ¡Ahora puedes ingresar al sistema!");
+		
+		return "redirect:/login";
+	}
+
+	@GetMapping("/about")
+	public String mostrarAcerca() {			
+		return "acerca";
+	}
+
+	@GetMapping("/login")
+	public String mostrarLogin() {
+		return "formLogin";
+	}
+
+	@GetMapping("/logout")
+	public String logout2(HttpServletRequest request) {
+		SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+		logoutHandler.logout(request, null, null);
+		return "redirect:/login";
+	}
+
+	@GetMapping("/logout2")
+	public String logout(HttpServletRequest request) {
+		SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+		logoutHandler.logout(request, null, null);
+		return "redirect:/";
+	}
 	
 	@ModelAttribute
 	public void getModelGenerico(Model model) {
